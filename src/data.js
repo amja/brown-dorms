07e207b1ada7f2987c292cd704419f4fd48d26ab
Hannah Chow <hannah_chow@brown.edu>
2017-12-07 12:12:51 -0500

    added content

--- src/data.js
+++ src/data.js
@@ -1,33 +1,105 @@
 const buildings = [
+
+	{
+		"name":"111 Brown Street",
+		"built":1900,
+		"roomType":[1, 2],
+		"accessible":"Non-accessible",
+		"numRooms": 7,
+		"kitchens": "General (1)",
+		"bathrooms": "Two per floor",
+		"program": "No",
+		"img":"111.jpg",
+		"description":"The wooden floors and fireplaces help you to forget that your roommate uses your toothbrush to clean his white sneakers.",
+		"navLink":"https://goo.gl/maps/d1feMnFYU4U2",
+		"latLong": "41.826134, -71.400644"
+	},
+
+	{
+		"name":"Barbour Hall",
+		"built":1904,
+		"roomType":[1, 2, 5],
+		"accessible":"Accessible",
+		"numRooms": 46,
+		"kitchens": "General (1) & In-Suite",
+		"bathrooms": "One per suite",
+		"program": "No",
+		"img":"barbour.jpg",
+		"description":"The wooden floors and fireplaces help you to forget that your roommate uses your toothbrush to clean his white sneakers.",
+		"navLink":"https://goo.gl/maps/d1feMnFYU4U2",
+		"latLong": "41.826134, -71.400644"
+	},
+	{
+		"name":"Buxton House",
+		"built":1951,
+		"roomType":[1,2],
+		"accessible":"Accessible",
+		"numRooms": "Rooms not granted through Housing Lottery",
+		"kitchens": "General (1)",
+		"bathrooms": "Two per floor",
+		"program": "International House",
+		"img":"buxton.jpg",
+		"description":"Residents feel on top of the world (if the world were a dining hall). This year, the Harvest Fest in the basement didn't have chicken pot pie.",
+		"navLink":"https://goo.gl/maps/M4zpGo4Cwp82",
+		"latLong": "41.830443, -71.402504"
+	},
 	{
 		"name":"Caswell Hall",
 		"built":1903,
 		"roomType":[2],
-		"accessible":false,
-		"img":"BrownUniversity-CaswellHall.jpg",
-		"description":"The wooden floors and fireplaces help you to forget that your roommate uses your toothbrush to clean his white sneakers.",
-		"googleMap":"https://goo.gl/maps/d1feMnFYU4U2",
-		"brownMap":"https://www.brown.edu/Facilities/Facilities_Management/maps/#building/CASWELL",
-		"latLong": "41.826134, -71.400644"
-	},
-	{
-		"name":"Andrews Hall",
-		"built":1927,
-		"roomType":[2,3],
-		"accessible":true,
-		"img":"BrownUniversity-AndrewsHall.jpg",
-		"description":"Residents feel on top of the world (if the world were a dining hall). This year, the Harvest Fest in the basement didn't have chicken pot pie.",
-		"googleMap":"https://goo.gl/maps/M4zpGo4Cwp82",
-		"latLong": "41.830443, -71.402504"
-	},
-	{
-		"name":"Marcy House",
-		"built":1952,
-		"roomType":[1,2],
-		"accessible":true,
-		"img":"BrownUniversity-MarcyHouse.jpg",
+		"accessible":"Non-Accessible",
+		"numRooms": 45,
+		"kitchens": "General (1)",
+		"bathrooms": "1 per four doubles",
+		"program": "No",
+		"img":"caswell.jpg",
 		"description":"Named for William Marcy, class of 1808, who became the Comptroller of the State of New York. I didn't think they had computers back in 1808 but my history knowledge is fuzzy. Houses the Jewish fraternity but not many Jews.",
-		"googleMap":"https://goo.gl/maps/CKczionqspj",
+		"navLink":"https://goo.gl/maps/CKczionqspj",
+		"latLong":"41.824878, -71.401378"
+	},
+
+	{
+		"name":"Chapin House",
+		"built":1951,
+		"roomType":[1,2],
+		"accessible":"Accessible",
+		"numRooms": 58,
+		"kitchens": "General (3)",
+		"bathrooms": "2 per floor",
+		"program": "Theta Delta Chi Fraternity & Harambee House",
+		"img":"chapin.jpg",
+		"description":"Named for William Marcy, class of 1808, who became the Comptroller of the State of New York. I didn't think they had computers back in 1808 but my history knowledge is fuzzy. Houses the Jewish fraternity but not many Jews.",
+		"navLink":"https://goo.gl/maps/CKczionqspj",
+		"latLong":"41.824878, -71.401378"
+	},
+
+	{
+		"name":"Diman House",
+		"built":1951,
+		"roomType":[1,2,3],
+		"accessible":"Accessible",
+		"numRooms": 44,
+		"kitchens": "General (3)",
+		"bathrooms": "2 per floor",
+		"program": "Kappa Alpha Theta Sorority & Social Action House",
+		"img":"diman.jpg",
+		"description":"Named for William Marcy, class of 1808, who became the Comptroller of the State of New York. I didn't think they had computers back in 1808 but my history knowledge is fuzzy. Houses the Jewish fraternity but not many Jews.",
+		"navLink":"https://goo.gl/maps/CKczionqspj",
+		"latLong":"41.824878, -71.401378"
+	},
+
+	{
+		"name":"Goddard House",
+		"built":1951,
+		"roomType":[1,2],
+		"accessible":"Accessible",
+		"numRooms": 91,
+		"kitchens": "General (3)",
+		"bathrooms": "2 per floor",
+		"program": "Alpha Delta Phi Fraternity",
+		"img":"goddard.jpg",
+		"description":"Named for William Marcy, class of 1808, who became the Comptroller of the State of New York. I didn't think they had computers back in 1808 but my history knowledge is fuzzy. Houses the Jewish fraternity but not many Jews.",
+		"navLink":"https://goo.gl/maps/CKczionqspj",
 		"latLong":"41.824878, -71.401378"
 	}
 ]
