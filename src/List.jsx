578c0921f1894f7fa14fa1e9365c53b9934e014e
amja <me@amosjackson.com>
2017-12-07 14:28:33 -0500

    Fixed class stuff

--- src/List.jsx
+++ src/List.jsx
@@ -9,14 +9,14 @@ class List extends Component {
             return (
                 <li key={item.name}>
                     <img alt={"photo of " + item.name} src={item.img}/>
-                    <h3><i class="fa fa-home"></i> {item.name}</h3>
+                    <h3><i className="fa fa-home"></i> {item.name}</h3>
                     <h4>Built: {item.built}</h4>{distance}
                     <p>{item.description} (map: <a href={item.brownMap} target="_blank">Brown</a>)</p>
-                    <p><span><i class="fa fa-users"></i>    Number of Rooms:</span> {item.numRooms == -1 ? "Rooms not granted through Housing Lottery" : item.numRooms}</p>
-                    <p><span><i class="fa fa-wheelchair"></i>    Accessibility:</span> {item.accessible ? "Accessible" : "Non-Accessible"}</p>
-                    <p><span><i class="fa fa-cutlery"></i>    Kitchens:</span> {item.kitchens}</p>
-                    <p><span><i class="fa fa-bath"></i>    Bathroom Type:</span> {item.bathrooms}</p>
-                    <p><span><i class="fa fa-institution"></i>    Program Housing?</span> {item.program}</p>
+                    <p><span><i className="fa fa-users"></i>    Number of Rooms:</span> {item.numRooms == -1 ? "Rooms not granted through Housing Lottery" : item.numRooms}</p>
+                    <p><span><i className="fa fa-wheelchair"></i>    Accessibility:</span> {item.accessible ? "Accessible" : "Non-Accessible"}</p>
+                    <p><span><i className="fa fa-cutlery"></i>    Kitchens:</span> {item.kitchens}</p>
+                    <p><span><i className="fa fa-bath"></i>    Bathroom Type:</span> {item.bathrooms}</p>
+                    <p><span><i className="fa fa-institution"></i>    Program Housing?</span> {item.program}</p>
                 </li>)
         });
 
