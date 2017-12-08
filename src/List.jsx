86bd6deafc4f83a52496833ea61201ec262abe1d
amja <me@amosjackson.com>
2017-12-07 20:19:23 -0500

    Removed non-standard tags

--- src/List.jsx
+++ src/List.jsx
@@ -7,13 +7,13 @@ class List extends Component {
     renderList() {
         const items = this.props.items.map(item => {
             if(item.time_text != null) {
-                var distance = <littlebox2><h5>Walking time to location: {item.time_text}</h5></littlebox2>
+                var distance = <div id="walking"><h5>Walking time to location: {item.time_text}</h5></div>
             }
             return (
                 <li key={item.name}>
                     <img alt={"photo of " + item.name} src={item.img}/>
                     <h3><i className="fa fa-home"></i> {item.name}</h3>
-                    <littlebox><h4><i className="fa fa-map-marker"></i> <a href={item.brownMap} target="_blank"> Map</a></h4></littlebox>
+                    <div id="map-box"><h4><i className="fa fa-map-marker"></i> <a href={item.brownMap} target="_blank"> Map</a></h4></div>
                     {distance}
                     <p><span><i className="fa fa-building-o"></i>  Built:</span> {item.built}</p>
                     <p><span><i className="fa fa-users"></i>    Number of Rooms:</span> {item.numRooms === -1 ? "Rooms not granted through Housing Lottery" : item.numRooms}</p>
